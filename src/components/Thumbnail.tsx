c275f6db147aa3f5b598ba091fd7c5816764a62e
nguyenhoangquan <quqn100dd@gmail.com>
2024-11-26 23:40:36 +0700

    fix

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -17,18 +17,20 @@ const Thumbnail = ({ url }: ThumbnailProps) => {
         <Dialog>
             <DialogTrigger>
                 <DialogTitle className="relative overflow-hidden max-w-[360px] border rounded-md my-1 cursor-zoom-in">
-                    <img
+                    <Image
                         src={url}
                         className="rounded-md size-full object-cover"
                         alt="thumbnail"
+                        fill
                     />
                 </DialogTitle>
             </DialogTrigger>
             <DialogContent className="max-w-[600px] border-none p-0 shadow-none">
-                <img
+                <Image
                     src={url}
                     className="rounded-md size-full object-cover"
                     alt="thumbnail"
+                    fill
                 />
             </DialogContent>
         </Dialog>
